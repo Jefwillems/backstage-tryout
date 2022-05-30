50850cd7303cccd5c513107d9e624bf8db7edad1
Jefwillems <jefwillems100@gmail.com>
2022-05-30 12:57:22 +0200

    remove sample data

--- packages/backend/src/plugins/auth.ts
+++ packages/backend/src/plugins/auth.ts
@@ -1,4 +1,8 @@
-import {createRouter, defaultAuthProviderFactories, providers} from '@backstage/plugin-auth-backend';
+import {
+  createRouter,
+  defaultAuthProviderFactories,
+  providers
+} from '@backstage/plugin-auth-backend';
 import {Router} from 'express';
 import {PluginEnvironment} from '../types';
 
@@ -13,46 +17,7 @@ export default async function createPlugin(
     tokenManager: env.tokenManager,
     providerFactories: {
       ...defaultAuthProviderFactories,
-      github: providers.github.create({
-        signIn: {
-          resolver:
-            async (info, ctx) => {
-              const {
-                profile: {email},
-              } = info;
-              // Profiles are not always guaranteed to to have an email address.
-              // You can also find more provider-specific information in `info.result`.
-              // It typically contains a `fullProfile` object as well as ID and/or access
-              // tokens that you can use for additional lookups.
-              if (!email) {
-                throw new Error('User profile contained no email');
-              }
-
-              // This example resolver simply uses the local part of the email as the name.
-              const [name] = email.split('@');
-
-              // This helper function handles sign-in by looking up a user in the catalog.
-              // The lookup can be done either by reference, annotations, or custom filters.
-              //
-              // The helper also issues a token for the user, using the standard group
-              // membership logic to determine the ownership references of the user.
-              return ctx.signInWithCatalogUser({
-                entityRef: {name},
-              });
-            }
-        },
-        authHandler: async (input) => {
-          const username = input.fullProfile.username;
-          const {photos} = input.fullProfile;
-          return {
-            profile: {
-              displayName: username,
-              email: input.fullProfile.emails?.join(';'),
-              picture: photos ? photos[0].value : undefined
-            }
-          }
-        }
-      })
+      github: providers.github.create()
     }
   });
 }
